745c5586afc7bfc171536892a6bcf6f176bbc0e4
tandlemuskaan <tandlemuskaan@gmail.com>
2026-07-04 23:03:29 +0530

    Add story code

---
+++ script.js
@@ -0,0 +1,20 @@
+let adjective = "majestic";
+let noun="peanutbottle";
+let verb="breathing out";
+let place="the hills";
+let adjective2="fierce";
+let noun2="bones";
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "happy";
+noun = "bojack";
+verb = "sad";
+place = "boat";
+adjective2 = "big";
+noun2 = "pop tarts";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
